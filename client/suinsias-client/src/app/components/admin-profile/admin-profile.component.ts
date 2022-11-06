19c0284744c6783ee536d7bafd6884825148fe52
Med Amine Lyazrhi <86062126+AmineLyazrhi@users.noreply.github.com>
2022-11-06 01:17:45 +0100

    Add files via upload

---
+++ client/suinsias-client/src/app/components/admin-profile/admin-profile.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+
+@Component({
+  selector: 'app-admin-profile',
+  templateUrl: './admin-profile.component.html',
+  styleUrls: ['./admin-profile.component.scss']
+})
+export class AdminProfileComponent implements OnInit {
+
+  constructor(private auth: AuthenticationService,private router:Router) { }
+
+
+  ngOnInit() {
+  }
+
+}
